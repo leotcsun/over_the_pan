b00e42986d7dc9112a68ab2ae2f4085f94a5f114
Leo Sun <shinji_97@hotmail.com>
2012-06-05 00:47:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OverThePan::Application.config.secret_token = 'cb3a3cb4c297834898a144c4ab13e9689822ac1aa32dcea90ac23efb6521c47be806c464ad78c0e59f3eb1562060f2bf5f742030cfd96d22615c0341e70ac529'
