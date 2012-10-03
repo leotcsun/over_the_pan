ca45e0715459c1d37b87464ed112d876ae6e9cd7
Leo Sun <leo.tcsun@gmail.com>
2012-10-03 03:37:47 -0400

    setting up redistogo on heroku

--- app/workers/sync_worker.rb
+++ app/workers/sync_worker.rb
@@ -1,7 +1,9 @@
 class SyncWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform(celebrity_id, full_sync)
+    full_sync ||= false
     celebrity = Celebrity.find_by_id(celebrity_id)
     celebrity.synchornize_post(full_sync: full_sync)
   end
